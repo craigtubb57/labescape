6f3f9ca028e6ab75fbe3c7c4331316ca88618dac
craigtubb57 <craig.tubb@mydrivesolutions.com>
2017-06-02 11:57:40 +0100

    RouteFinderTest and RouteFinder class with first cut of algorithm (not compiling)

---
+++ src/main/java/com/craigtubb/RouteFinder.java
@@ -0,0 +1,36 @@
+package com.craigtubb;
+
+import java.util.AbstractMap;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+public class RouteFinder {
+
+    private Labyrinth labyrinth;
+    private int startX;
+    private int startY;
+
+    public RouteFinder(Labyrinth labyrinth, int startX, int startY) {
+        this.labyrinth = labyrinth;
+        this.startX = startX;
+        this.startY = startY;
+    }
+
+    public Corridor find() throws NoEscapeException {
+        Corridor startCorridor = new Corridor(startX, startY, findNext(startX, startY));
+        if (startCorridor.getNext() == null) {
+            throw new NoEscapeException();
+        }
+        return startCorridor;
+    }
+
+    private Corridor findNext(int x, int y) throws NoEscapeException {
+        return IntStream.rangeClosed(x - 1, x + 1).boxed().flatMap(
+                row -> IntStream.rangeClosed(y - 1, y + 1).boxed().map(col -> new AbstractMap.SimpleEntry<>(row, col)))
+                .filter(pair -> labyrinth.hasPoint(pair.getKey(), pair.getValue())).map(pair -> findNext(pair.getKey(), pair.getValue())).findFirst().orElseThrow(noEscape());
+    }
+    
+    private static Supplier<NoEscapeException> noEscape() {
+        return () -> new NoEscapeException();
+    }
+}
