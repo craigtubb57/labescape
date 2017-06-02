e9fdf0a2660bec6f95afc0500d400830053bbe0d
craigtubb57 <craig.tubb@mydrivesolutions.com>
2017-06-02 14:24:21 +0100

    Added more RouteFinderTests and improved algorithm concerning previous labyrinth points

--- src/main/java/com/craigtubb/RouteFinder.java
+++ src/main/java/com/craigtubb/RouteFinder.java
@@ -1,10 +1,11 @@
 package com.craigtubb;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import static java.util.AbstractMap.SimpleEntry;
 
 public class RouteFinder {
 
@@ -22,29 +23,33 @@ public class RouteFinder {
 
     public Corridor find() throws NoEscapeException {
         SimpleEntry<Integer, Integer> start = new SimpleEntry<>(startX, startY);
-        SimpleEntry<Integer, Integer> none = new SimpleEntry<>(-1, -1);
-        Corridor startCorridor = new Corridor(startX, startY, findNext(start, none));
+        Corridor startCorridor = new Corridor(startX, startY, findNext(start, new ArrayList<>()));
         if (startCorridor.getNext() == null) {
             throw new NoEscapeException();
         }
         return startCorridor;
     }
 
-    private Corridor findNext(SimpleEntry<Integer, Integer> at, SimpleEntry<Integer, Integer> ignoring) {
-        Corridor next = adjacent(at).get().filter(pair -> !pair.equals(ignoring)).filter(pair -> labyrinth.hasCharAtPoint(freeChar, pair.getKey(), pair.getValue())).map(pair -> new Corridor(pair.getKey(), pair.getValue(), findNext(pair, at))).peek(System.out::println).findFirst().orElse(null);
-        
+    private Corridor findNext(SimpleEntry<Integer, Integer> at, List<SimpleEntry<Integer, Integer>> previous) {
+        previous.add(at);
+        Corridor next = adjacent(at).get().filter(pair -> !previous.contains(pair))
+                .filter(pair -> labyrinth.hasCharAtPoint(freeChar, pair.getKey(), pair.getValue()))
+                .map(pair -> new Corridor(pair.getKey(), pair.getValue(), findNext(pair, previous))).findFirst()
+                .orElse(null);
+
         if (next != null && (next.getNext() != null || labyrinth.isExit(next.getX(), next.getY()))) {
             return next;
         }
         return null;
     }
-    
+
     private Supplier<Stream<SimpleEntry<Integer, Integer>>> adjacent(SimpleEntry<Integer, Integer> to) {
         int x = to.getKey();
         int y = to.getValue();
-        return () -> Stream.of(new SimpleEntry<>(x - 1, y), new SimpleEntry<>(x + 1, y), new SimpleEntry<>(x, y - 1), new SimpleEntry<>(x, y + 1));
+        return () -> Stream.of(new SimpleEntry<>(x - 1, y), new SimpleEntry<>(x, y + 1), new SimpleEntry<>(x + 1, y),
+                new SimpleEntry<>(x, y - 1));
     }
-    
+
     private static Supplier<NoEscapeException> noEscape() {
         return () -> new NoEscapeException();
     }
--- src/test/java/com/craigtubb/RouteFinderTest.java
+++ src/test/java/com/craigtubb/RouteFinderTest.java
@@ -1,9 +1,7 @@
 package com.craigtubb;
 
 import static com.craigtubb.LabEscape.FREE;
-import static com.craigtubb.TestHelper.labyrinthWithExit;
-import static com.craigtubb.TestHelper.labyrinthWithoutExit;
-import static com.craigtubb.TestHelper.route;
+import static com.craigtubb.TestHelper.*;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -11,8 +9,32 @@ import org.junit.Test;
 public class RouteFinderTest {
 
     @Test
-    public void withExit() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithExit()), FREE, 3, 1);
+    public void withClearExit() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExit()), FREE, 3, 1);
+        Corridor result = finder.find();
+        
+        assertEquals(route(), result);
+    }
+    
+    @Test
+    public void withUnclearExit() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithUnclearExit()), FREE, 3, 1);
+        Corridor result = finder.find();
+        
+        assertEquals(route(), result);
+    }
+    
+    @Test
+    public void withClearExitAndClosedLoop() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndClosedLoop()), FREE, 3, 1);
+        Corridor result = finder.find();
+        
+        assertEquals(route(), result);
+    }
+    
+    @Test
+    public void withClearExitAndOpenLoop() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndOpenLoop()), FREE, 3, 1);
         Corridor result = finder.find();
         
         assertEquals(route(), result);
@@ -21,6 +43,18 @@ public class RouteFinderTest {
     @Test(expected = NoEscapeException.class)
     public void withoutExit() throws NoEscapeException {
         RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExit()), FREE, 3, 1);
+        finder.find();
+    }
+    
+    @Test(expected = NoEscapeException.class)
+    public void withoutExitAndClosedLoop() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndClosedLoop()), FREE, 3, 1);
+        finder.find();
+    }
+    
+    @Test(expected = NoEscapeException.class)
+    public void withoutExitAndOpenLoop() throws NoEscapeException {
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndOpenLoop()), FREE, 3, 1);
         finder.find();
     }
 }
