acc9f1d9ae3fb2842db319f7575d4cf0b81d869d
craigtubb57 <craig.tubb@mydrivesolutions.com>
2017-06-03 09:20:54 +0100

    Remove given path char from RoutePainter and add comments

--- src/main/java/com/craigtubb/RoutePainter.java
+++ src/main/java/com/craigtubb/RoutePainter.java
@@ -1,27 +1,44 @@
 package com.craigtubb;
 
+import com.craigtubb.domain.Corridor;
+import com.craigtubb.domain.Labyrinth;
+
+/**
+ * Replaces free points in a given Labyrinth with path points, following a chain of Corridors starting with the given
+ * Corridor.
+ * 
+ * @author craigtubb
+ *
+ */
 public class RoutePainter {
 
     private Labyrinth labyrinth;
     private Corridor routeStart;
-    private char pathChar;
-    
-    public RoutePainter(Labyrinth labyrinth, Corridor routeStart, char pathChar) {
+
+    public RoutePainter(Labyrinth labyrinth, Corridor routeStart) {
         this.labyrinth = labyrinth;
         this.routeStart = routeStart;
-        this.pathChar = pathChar;
     }
-    
+
+    /**
+     * Replaces free points in the Labyrinth with paths indicated by the starting Corridor.
+     * 
+     * @return The given Labyrinth with a path 'painted' on it.
+     */
     public Labyrinth paint() {
         paintCorridor(routeStart);
         return labyrinth;
     }
-    
+
+    /*
+     * Replace a free point in the given Labyrinth with a path specified by the given Corridor.
+     */
     private void paintCorridor(Corridor corridor) {
-        if (corridor != null ) {
-            labyrinth.setPoint(corridor.getX(), corridor.getY(), pathChar);
+        if (corridor != null) {
+            labyrinth.setPath(corridor.getPoint());
+            // Recurse and paint the next corridor
             paintCorridor(corridor.getNext());
         }
-        
+
     }
 }
--- src/test/java/com/craigtubb/RoutePainterTest.java
+++ src/test/java/com/craigtubb/RoutePainterTest.java
@@ -4,15 +4,16 @@ import static com.craigtubb.TestHelper.labyrinthWithClearExit;
 import static com.craigtubb.TestHelper.labyrinthWithClearRoute;
 import static com.craigtubb.TestHelper.route;
 import static org.junit.Assert.assertEquals;
-import static com.craigtubb.LabEscape.*;
 
 import org.junit.Test;
+
+import com.craigtubb.domain.Labyrinth;
 
 public class RoutePainterTest {
 
     @Test
     public void withExit() {
-        RoutePainter painter = new RoutePainter(new Labyrinth(labyrinthWithClearExit()), route(), PATH);
+        RoutePainter painter = new RoutePainter(new Labyrinth(labyrinthWithClearExit()), route());
         Labyrinth result = painter.paint();
 
         assertEquals(new Labyrinth(labyrinthWithClearRoute()), result);
