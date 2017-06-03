f3bb816ef476cf5c4ca354e2e0df2bbe302e1ae9
craigtubb57 <craig.tubb@mydrivesolutions.com>
2017-06-03 09:21:21 +0100

    Simplify RouteFinder and add comments

--- src/main/java/com/craigtubb/RouteFinder.java
+++ src/main/java/com/craigtubb/RouteFinder.java
@@ -1,53 +1,108 @@
 package com.craigtubb;
 
-import java.util.AbstractMap.SimpleEntry;
+import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import com.craigtubb.domain.Corridor;
+import com.craigtubb.domain.Labyrinth;
+import com.craigtubb.exception.NoEscapeException;
+
+/**
+ * Finds a route of free points in a Labyrinth, returning a Corridor representing the starting Point.
+ * 
+ * @author craigtubb
+ *
+ */
 public class RouteFinder {
 
     private Labyrinth labyrinth;
-    private char freeChar;
     private int startX;
     private int startY;
 
-    public RouteFinder(Labyrinth labyrinth, char freeChar, int startX, int startY) {
+    public RouteFinder(Labyrinth labyrinth, int startX, int startY) {
         this.labyrinth = labyrinth;
-        this.freeChar = freeChar;
         this.startX = startX;
         this.startY = startY;
     }
 
+    /**
+     * Find the first route through the Labyrinth, returning a Corridor at the starting point. Directions are checked in
+     * order of N, E, S, W.
+     * 
+     * @return A new Corridor at the starting point.
+     * @throws NoEscapeException
+     *             If no route to the exit was found.
+     */
     public Corridor find() throws NoEscapeException {
-        SimpleEntry<Integer, Integer> start = new SimpleEntry<>(startX, startY);
-        Corridor startCorridor = new Corridor(startX, startY, findNext(start, new ArrayList<>()));
-        if (startCorridor.getNext() == null) {
-            throw new NoEscapeException();
-        }
-        return startCorridor;
+        return Stream.of(new Point(startX, startY)).map(next()).filter(onRoute()).findFirst().orElseThrow(noEscape());
     }
 
-    private Corridor findNext(SimpleEntry<Integer, Integer> at, List<SimpleEntry<Integer, Integer>> previous) {
-        previous.add(at);
-        Corridor next = adjacent(at).get().filter(pair -> !previous.contains(pair))
-                .filter(pair -> labyrinth.hasCharAtPoint(freeChar, pair.getKey(), pair.getValue()))
-                .map(pair -> new Corridor(pair.getKey(), pair.getValue(), findNext(pair, previous))).findFirst()
+    private Function<Point, Corridor> next() {
+        return next(new ArrayList<>());
+    }
+
+    /*
+     * Next point as a Corridor, recursing to populate the next next points as a chain.
+     */
+    private Function<Point, Corridor> next(List<Point> past) {
+        return point -> new Corridor(point, findNext(point, past));
+    }
+
+    /**
+     * Finds the next Corridor on the exit route from the given Point.
+     * 
+     * Algorithm:
+     * <ul>
+     * <li>Add given Point to past point list.</li>
+     * <li>Get NSEW points from given Point.</li>
+     * <li>Filter out those in the past.</li>
+     * <li>Filter out those that are not free.</li>
+     * <li>Map to next Corridor point candidates using recursion.</li>
+     * <li>Filter out those that are not on an exit route.</li>
+     * <li>Return the first next Corridor, or null if there are none.</li>
+     * </ul>
+     * 
+     * @param at
+     *            The Point from which to find the next Corridor.
+     * @param past
+     *            All the Points which have been previously analysed.
+     * @return A new Corridor representing the next point on the exit route, or null if there is none.
+     **/
+    private Corridor findNext(Point at, List<Point> past) {
+        past.add(at);
+        Predicate<Point> contains = past::contains;
+        Predicate<Point> free = labyrinth::freeAtPoint;
+        // get NSEW Points, filter those from the past and those not free, map to
+        return adjacent(at).get().filter(contains.negate()).filter(free).map(next(past)).filter(onRoute()).findFirst()
                 .orElse(null);
-
-        if (next != null && (next.getNext() != null || labyrinth.isExit(next.getX(), next.getY()))) {
-            return next;
-        }
-        return null;
     }
 
-    private Supplier<Stream<SimpleEntry<Integer, Integer>>> adjacent(SimpleEntry<Integer, Integer> to) {
-        int x = to.getKey();
-        int y = to.getValue();
-        return () -> Stream.of(new SimpleEntry<>(x - 1, y), new SimpleEntry<>(x, y + 1), new SimpleEntry<>(x + 1, y),
-                new SimpleEntry<>(x, y - 1));
+    /*
+     * Supplies a Stream of new Points representing N, E, S, W from the given point, in that order.
+     */
+    private Supplier<Stream<Point>> adjacent(Point to) {
+        // @formatter:off
+        // X =        N  E  S  W
+        // Y =           N  E  S   W
+        int[] os = { -1, 0, 1, 0, -1 };
+        // @formatter:on
+
+        // For i = 0-4, returns new Point where x = os[i] and y = os[i + 1]
+        return () -> IntStream.range(0, 4).boxed().map(i -> new Point(to.x + os[i], to.y + os[i + 1]));
+    }
+
+    /*
+     * A given Corridor is potentially on an exit route if it's next Corridor is on an exit route, or it represents the
+     * exit itself.
+     */
+    private Predicate<Corridor> onRoute() {
+        return next -> next.getNext() != null || labyrinth.isExit(next.getPoint());
     }
 
     private static Supplier<NoEscapeException> noEscape() {
--- src/test/java/com/craigtubb/RouteFinderTest.java
+++ src/test/java/com/craigtubb/RouteFinderTest.java
@@ -1,60 +1,70 @@
 package com.craigtubb;
 
-import static com.craigtubb.LabEscape.FREE;
-import static com.craigtubb.TestHelper.*;
+import static com.craigtubb.TestHelper.labyrinthWithClearExit;
+import static com.craigtubb.TestHelper.labyrinthWithClearExitAndClosedLoop;
+import static com.craigtubb.TestHelper.labyrinthWithClearExitAndOpenLoop;
+import static com.craigtubb.TestHelper.labyrinthWithUnclearExit;
+import static com.craigtubb.TestHelper.labyrinthWithoutExit;
+import static com.craigtubb.TestHelper.labyrinthWithoutExitAndClosedLoop;
+import static com.craigtubb.TestHelper.labyrinthWithoutExitAndOpenLoop;
+import static com.craigtubb.TestHelper.route;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import com.craigtubb.domain.Corridor;
+import com.craigtubb.domain.Labyrinth;
+import com.craigtubb.exception.NoEscapeException;
 
 public class RouteFinderTest {
 
     @Test
     public void withClearExit() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExit()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExit()), 3, 1);
         Corridor result = finder.find();
-        
+
         assertEquals(route(), result);
     }
-    
+
     @Test
     public void withUnclearExit() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithUnclearExit()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithUnclearExit()), 3, 1);
         Corridor result = finder.find();
-        
+
         assertEquals(route(), result);
     }
-    
+
     @Test
     public void withClearExitAndClosedLoop() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndClosedLoop()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndClosedLoop()), 3, 1);
         Corridor result = finder.find();
-        
+
         assertEquals(route(), result);
     }
-    
+
     @Test
     public void withClearExitAndOpenLoop() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndOpenLoop()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithClearExitAndOpenLoop()), 3, 1);
         Corridor result = finder.find();
-        
+
         assertEquals(route(), result);
     }
 
     @Test(expected = NoEscapeException.class)
     public void withoutExit() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExit()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExit()), 3, 1);
         finder.find();
     }
-    
+
     @Test(expected = NoEscapeException.class)
     public void withoutExitAndClosedLoop() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndClosedLoop()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndClosedLoop()), 3, 1);
         finder.find();
     }
-    
+
     @Test(expected = NoEscapeException.class)
     public void withoutExitAndOpenLoop() throws NoEscapeException {
-        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndOpenLoop()), FREE, 3, 1);
+        RouteFinder finder = new RouteFinder(new Labyrinth(labyrinthWithoutExitAndOpenLoop()), 3, 1);
         finder.find();
     }
 }
