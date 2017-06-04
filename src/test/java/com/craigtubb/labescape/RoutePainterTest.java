850bcbf717ccfe616aa0889e70a495872f871c7b
craigtubb57 <craig.tubb@mydrivesolutions.com>
2017-06-04 10:34:37 +0100

    Format imports on various files

--- src/test/java/com/craigtubb/labescape/RoutePainterTest.java
+++ src/test/java/com/craigtubb/labescape/RoutePainterTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.craigtubb.domain.Labyrinth;
-import com.craigtubb.labescape.RoutePainter;
 
 public class RoutePainterTest {
 
